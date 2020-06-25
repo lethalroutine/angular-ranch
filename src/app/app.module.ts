24e40d39df54b26800e3536fc3429f2c78fbdb44
Olek Gajda <aleksander.gajda1@gmail.com>
2020-06-25 17:09:53 +0200

    Add log in form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     AppRoutingModule,
     BrowserModule,
     RouterModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
-
-  constructor() { }
+  user: User = {email: null, password: null};
+  submitted = false;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm): void {
+    const user = form.value as User;
+    this.submitted = true;
+    console.log(`submitted ${user.email}`);
+    form.reset();
+    this.router.navigate(['map']);
+  }
+}
+
+interface User {
+  email: string;
+  password: string;
 }
