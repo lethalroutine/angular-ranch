d8daa259daa957db7b9b6d29678a0582580b71be
Olek Gajda <aleksander.gajda1@gmail.com>
2020-06-25 17:42:49 +0200

    Add sign up form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  user: RegistrationUser = {email: null, password: null, retypedPassword: null};
+  submitted = false;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm) {
+    const user = form.value as RegistrationUser;
+    this.submitted = true;
+    console.log(`submitted ${user.email}`);
+    if (user.password !== user.retypedPassword) {
+      return;
+    }
+    form.reset();
+    this.router.navigate(['log-in']);
+  }
+}
+
+interface RegistrationUser {
+  email: string;
+  password: string;
+  retypedPassword: string;
 }
