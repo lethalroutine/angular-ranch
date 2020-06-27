4831d7af329b94da3237c12f0cc83dd062e38e90
Olek Gajda <aleksander.gajda1@gmail.com>
2020-06-27 17:49:26 +0200

    Add drawing of polygons on the map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,8 +1,9 @@
-import {AfterViewInit, Component} from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 import * as L from 'leaflet';
-import {DataProviderService} from '../_services/data-provider.service';
-import {AccountService} from '../_services/account.service';
+import { DataProviderService } from '../_services/data-provider.service';
+import { AccountService } from '../_services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-map',
@@ -15,10 +16,11 @@ export class MapComponent implements AfterViewInit {
 
   constructor(
     private dataService: DataProviderService,
-    private accountService: AccountService
-    ) {
+    private accountService: AccountService,
+    private router: Router
+  ) {
     this.accountService.isUserAuthenticated.subscribe((value) =>
-    this.isUserLoggedIn = value);
+      this.isUserLoggedIn = value);
   }
 
   ngAfterViewInit(): void {
@@ -27,7 +29,7 @@ export class MapComponent implements AfterViewInit {
 
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
+      center: [39.8282, -98.5795],
       zoom: 3
     });
 
@@ -40,9 +42,23 @@ export class MapComponent implements AfterViewInit {
   }
 
   onClick(id: number) {
-    this.dataService.fetchDataFromServer(id).subscribe((data) =>
-    {
-      console.log(data);
-    });
+    this.dataService.fetchDataFromServer(id).subscribe((data) => {
+      this.drawPolygon(data);
+    }, (error => {
+      this.handleExpiredToken();
+    }));
+  }
+
+  private drawPolygon(data) {
+    const corners = data['corners'];
+    const polygonData = corners.map(val => [val['lat'], val['lng']]);
+    const polygon = L.polygon(polygonData, {color: 'red'});
+    polygon.addTo(this.map);
+  }
+
+  private handleExpiredToken() {
+    window.alert('Token has expired');
+    this.accountService.isUserAuthenticated.next(false);
+    this.router.navigate(['log-in']);
   }
 }
